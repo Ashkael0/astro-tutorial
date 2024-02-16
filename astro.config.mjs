55499907ae695858d03f2e7a6642456e753435ef
KeSat <kesat.github@gmail.com>
2024-02-16 15:41:51 +0000

    First island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://master--bespoke-travesseiro-3452a8.netlify.app/'
-})
+  site: 'https://master--bespoke-travesseiro-3452a8.netlify.app/',
+  integrations: [preact()]
+});
